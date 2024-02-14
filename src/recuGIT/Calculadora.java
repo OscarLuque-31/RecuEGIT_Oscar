abad35a4d0896bba0ab07462c1e8382d7f797191
OscarLuque-31 <a_oscar.luque.hidalgo@iespablopicasso.es>
2024-02-14 09:47:18 +0100

    Subo codigo de calculadora

---
+++ src/recuGIT/Calculadora.java
@@ -0,0 +1,52 @@
+package recuGIT;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+  private static Scanner sc = new Scanner(System.in);
+  
+  public static void main(String[] args) {
+      
+    //Bucle de operaciones
+    int continuar = 1;
+    while(continuar != 0){
+      System.out.println("CALCULADORA");
+      System.out.println("-----------");
+      System.out.println("1.Suma");
+      System.out.println("2.Resta");
+      System.out.println("0.Salir de la aplicación");
+      continuar = Integer.parseInt(sc.nextLine());
+      
+      if(continuar == 1) {
+        System.out.print("Introduzca el primer numero:");
+        int numeroUno = Integer.parseInt(sc.nextLine());
+        System.out.print("Introduzca el segundo numero: ");
+        int numeroDos = Integer.parseInt(sc.nextLine());
+        
+        System.out.println( suma(numeroUno,numeroDos));
+
+        
+      }else if(continuar == 2) {
+        System.out.print("Introduzca el primer numero:");
+        System.out.print("Introduzca el segundo numero: ");
+      }
+      
+      
+      
+    }
+    
+    
+    
+
+  }
+
+  
+  private static int suma(int numeroUno,int numeroDos) {
+    return (numeroUno + numeroDos);
+  }
+  
+  private int resta(int numeroUno,int numeroDos) {
+    return (numeroUno - numeroDos);
+  }
+}
